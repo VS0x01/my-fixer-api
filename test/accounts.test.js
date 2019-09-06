c6787eb8eff1d98a0615c89470b6aeb46d3155a7
Vadim <vadim.a.shesterikov@gmail.com>
2019-09-06 12:39:52 +0300

    fix login test

--- test/accounts.test.js
+++ test/accounts.test.js
@@ -1,26 +1,27 @@
-const chai = require('chai')
-    , chaiHttp = require('chai-http')
-    , should = chai.should();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const should = chai.should();
 const app = require('../app');
 
 chai.use(chaiHttp);
 
 describe('Accounts', () => {
-    describe('#users.signIn()', () => {
-        it('should login user and return his data with auth tokens', (done) => {
-            chai.request(app)
-                .post('/accounts/sign-in')
-                .send({
-                    email: 'vadim.shesterikov@example.com',
-                    password: 'sdcddscscdsd',
-                })
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('user').that.has.keys(['fullName', 'email', 'photo']);
-                    res.body.should.have.property('accessToken');
-                    res.body.should.have.property('refreshToken');
-                    done();
-                });
+  describe('#users.signIn()', () => {
+    it('should login user and return his data with auth tokens', (done) => {
+      chai.request(app)
+        .post('/accounts/sign-in')
+        .send({
+          email: 'vadim.shesterikov@example.com',
+          password: 'sdcddscscdsd',
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('user').that.has.keys(['fullName', 'email', 'photo']);
+          res.body.should.have.property('accessToken');
+          res.body.should.have.property('refreshToken');
+          done();
         });
     });
+  });
 });
